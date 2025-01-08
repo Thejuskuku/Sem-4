85ff7f7f0a4e97a5dd3583a00084784ec62e6e94
Thejus S <143246271+Thejuskuku@users.noreply.github.com>
2025-01-08 15:15:21 +0530

    Add files via upload

--- Numerical Methods/Bisection.py
+++ Numerical Methods/Bisection.py
@@ -1,3 +1,5 @@
+import sympy as sp
+
 def bisection_method(func, a, b, tol=1e-6, max_iter=100):  
     # Ensure that f(a) and f(b) have opposite signs
     if func(a) * func(b) > 0:
@@ -20,9 +22,15 @@ def bisection_method(func, a, b, tol=1e-6, max_iter=100):
             break
     return (a + b) / 2.0
 
-if __name__ == "__main__":
-    def func(x):
-        return x**2 - x - 6
+def get_function_from_user():
+    func_input = input("Enter the function (in terms of x, e.g., 'x**2 - x - 6'): ")
+    x = sp.symbols('x')
+    func_expr = sp.sympify(func_input)
+    func_lambda = sp.lambdify(x, func_expr, 'numpy')
+    return func_lambda
+
+if __name__ == "__main__":     
+    func = get_function_from_user()
     a = eval(input("Enter the left endpoint : "))
     b = eval(input("Enter the right endpoint : "))
     tolerance = 1e-6
